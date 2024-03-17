77d9b5f77ab351f95f477c2b945bd18cbbde3fdf
Hyram Tang <hyram.tang@gmail.com>
2024-03-17 23:40:21 +0800

    feat: 添加 Element Plus

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,16 @@
-<script setup></script>
+<script setup>
+import { ElMessage } from 'element-plus'
+import { ref } from 'vue'
+
+const name = ref('hyram')
+
+const onClickSayHello = () => {
+  ElMessage({ message: `hello ${name.value}`, type: 'success' })
+}
+</script>
 
 <template>
-  <main>
-    <h1>这里是首页</h1>
-  </main>
+  <h1>这里是首页</h1>
+  <el-input v-model="name"></el-input>
+  <el-button type="success" @click="onClickSayHello">Say Hello</el-button>
 </template>
